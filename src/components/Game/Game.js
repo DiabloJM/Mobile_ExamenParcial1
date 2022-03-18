5d011732374dbf72dd2eba8322c23a0c502caab7
LAPTOP-5835AAD2\ferna <fernandojmichel@outlook.com>
2022-03-18 13:34:19 -0600

    function calculateText

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,10 +5,35 @@ class Game extends Component {
     constructor() {
         super()
         this.state = {
-            number: 0,
+            number: "",
             message: "",
             random: generateRandomNumber(100)
         }
+    }
+
+    handleOnChange = e => {
+        const {target: {value}} = e;
+        console.log(value);
+
+        if (value.trim() > 0)
+        {
+            this.setState({
+                number: value,
+            });
+        }
+
+    }
+
+    handleOnClick = () => {
+        const number = parseInt(this.state.number);
+        const random = parseInt(this.state.random);
+        console.log(random);
+
+        const text = calculateText(number, random);
+
+        this.setState({
+            message: text
+        })
     }
 
     render() {
@@ -16,10 +41,12 @@ class Game extends Component {
             <div className="Game">
                 <input
                     type="number"
+                    value = {this.state.number}
+                    onChange = {this.handleOnChange} 
                 />
 
-                <button>Probar</button>
-                <p>Texto</p>
+                <button onClick={this.handleOnClick}>Probar</button>
+                <p>{this.state.message}</p>
             </div>
         );
     }
@@ -29,4 +56,11 @@ export default Game;
 
 function generateRandomNumber(max, min=1) {
     return Math.floor(Math.random() *(max - min) + min);
+}
+
+function calculateText(number, random){
+    if(number === random) {
+        return "Felicidades, has acertado!!"
+    }
+    
 }
